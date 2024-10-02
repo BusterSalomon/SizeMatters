5aa4ef5ba87546824f08c1a1a0011a0c8fee4bcd
David Fuchs <104166797+fuchsdavid@users.noreply.github.com>
2024-10-02 10:59:14 +0200

    Add moving bar and ground

---
+++ Assets/Moving_platform_seven.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+
+
+public class Moving_platform_seven : MonoBehaviour
+{
+    public Transform posC, posD;
+    public float speed; 
+    Vector3 targetPos;
+
+private void Start()
+{
+    targetPos = posD.position;
+}
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, posC.position) < 0.05f) 
+        {
+            targetPos = posD.position;
+        }
+
+        if (Vector2.Distance(transform.position, posD.position) < 0.05f) 
+        {
+            targetPos = posC.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Character"))
+        {
+            collision.transform.parent=this.transform; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Character"))
+        {
+            collision.transform.parent=null; 
+        }
+    }
+}
