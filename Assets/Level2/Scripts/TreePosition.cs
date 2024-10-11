04f65fb67e3a57d5aed97fc47029e9f2314ce92f
BusterSalomon <bustersalomonrasmussen@gmail.com>
2024-10-11 16:37:47 +0200

    started level 2

---
+++ Assets/Level2/Scripts/TreePosition.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreePosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Camera main_camera;
+    void Start()
+    {
+        // Set size and position
+        main_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
