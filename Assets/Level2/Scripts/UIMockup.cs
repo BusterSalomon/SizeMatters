edc6b51d9c0051f0877c2582e6661e6f693422c4
David Fuchs <104166797+fuchsdavid@users.noreply.github.com>
2024-11-19 02:16:33 +0100

    LevelStructure(still in progress)
    I will ask our teacher tomorrow about the button bug

--- Assets/Level2/Scripts/UIMockup.cs
+++ Assets/Level2/Scripts/UIMockup.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIMockup : MonoBehaviour
 {
+
+    [SerializeField] private GameObject GameOverScreen;
+    [SerializeField] private GameObject NextLevel;
+
     private void Start()
     {
         
@@ -13,10 +18,23 @@ public class UIMockup : MonoBehaviour
     public void LogWin()
     {
         UnityEngine.Debug.Log("You won!");
+        NextLevel.SetActive(true);
     }
 
     public void LogLose()
     {
         UnityEngine.Debug.Log("You lost!");
+        GameOverScreen.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+      public void Home()
+    {
+        SceneManager.LoadScene(0);
     }
 }
+
