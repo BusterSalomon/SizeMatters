65c4609741c936dec5fdce85f8072e2fb49493bf
David Fuchs <104166797+fuchsdavid@users.noreply.github.com>
2024-12-11 14:27:04 +0100

    level manager -l2

--- Assets/Level2/Scripts/Level2Manager.cs
+++ Assets/Level2/Scripts/Level2Manager.cs
@@ -10,27 +10,33 @@ public class Level2Manager : LevelManager
     private Transform virusTransform;
     private Health characterHealth;
 
+    private Enemy enemy;
+
     public void Start()
     {
         GameObject jar = GameObject.Find("BugJar");
         jarLeftTransform = jar.transform.Find("Left");
         jarRightTransform = jar.transform.Find("Right");
         virusTransform = GameObject.Find("Bug").transform;
+        enemy = virusTransform.GetComponent<Enemy>();
         characterHealth = GameObject.FindGameObjectWithTag("Character").GetComponent<Health>();
         Debug.Log(characterHealth);
     }
     public override bool DidWin()
     {
-        float x = virusTransform.position.x;
-        float y = virusTransform.position.y;
-        float jlx = jarLeftTransform.position.x;
-        float jrx = jarRightTransform.position.x;
-        float jy = jarLeftTransform.position.y + jarLeftTransform.localScale.y/2;
-        return (x > jlx && x < jrx && y < jy);
+        if (virusTransform) {
+            float x = virusTransform.position.x;
+            float y = virusTransform.position.y;
+            float jlx = jarLeftTransform.position.x;
+            float jrx = jarRightTransform.position.x;
+            float jy = jarLeftTransform.position.y + jarLeftTransform.localScale.y/2;
+            return (x > jlx && x < jrx && y < jy);
+        }
+        else return false;
     }
 
     public override bool DidLose()
     {
-        return characterHealth.currentHealth <= 0;
+        return enemy.CurrentHealth <= 0;
     }
 }
