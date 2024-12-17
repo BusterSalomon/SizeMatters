9bc7167641d121e276a5eb473848faaba2b5363f
BusterSalomon <bustersalomonrasmussen@gmail.com>
2024-12-17 10:11:11 +0100

    removed stuff so it could be exported

--- Assets/Scripts/CustomCameraFollow.cs
+++ Assets/Scripts/CustomCameraFollow.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
 
 public class CustomCameraFollow : MonoBehaviour
